e1f88e78521aab26ba7d5203b12439552908ccfd
Xiaoping Tang <xiaoping.tang@gmail.com>
2023-11-12 23:35:14 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh # abort on errors
+
+set -e # build
+npm run docs:build # navigate into the build output directory
+cd .vitepress/dist # if you are deploying to a custom domain
+
+# echo 'www.example.com' > CNAME git init
+git add -A
+git commit -m 'deploy' # if you are deploying to https://<USERNAME>.github.io
+
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master 
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+
+git push -f git@github.com:RoadSideUniversity/roadsideuniversity.github.io.git master:gh-pages cd -
