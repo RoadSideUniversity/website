e980f624ef910196c983d7dbdc9e2e650338d005
Xiaoping Tang <xiaoping.tang@gmail.com>
2023-11-12 23:39:28 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,4 @@ git commit -m 'deploy' # if you are deploying to https://<USERNAME>.github.io
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 
-git push -f git@github.com:RoadSideUniversity/roadsideuniversity.github.io.git main
+git push -f git@github.com:RoadSideUniversity/roadsideuniversity.github.io.git main:gh-pages
